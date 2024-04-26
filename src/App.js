79891c2708b35235f0e5120a6b73c8f939ff5042
Joey Stipich <stipichj@gmail.com>
2024-04-26 18:09:31 -0400

    EOD commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import * as d3 from "d3";
 import './App.css';
+import { useEffect } from "react";
+import { useRef } from "react";
 
 function App() {
+  const ref = useRef()
 
-  const svg = d3.select("div")
-    .append("svg")
-    .attr("width", 50)
-    .attr("height", 100)
+  /* useEffect(()=>{
+    const svgElement = d3.select(ref.current)
+    svgElement.append("svg")
+      .attr("x", 150)
+      .attr("y", 200)
+      .attr("width", 50)
+      .attr("height", 100)
+      .attr("fill", "")
+  }, []) */
 
   return (
-    <div className="App">
-      
-    </div>
+    <svg>
+      <rect
+        x="150"
+        y="77"
+        width="40"
+        height="100"
+      />
+    </svg>
   );
 }
 
