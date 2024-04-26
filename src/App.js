4f3dfea9f5632fceb093790a70d7b24f95c13886
Joey Stipich <stipichj@gmail.com>
2024-04-26 13:29:59 -0400

    install D3 via npm

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import * as d3 from "d3";
 import './App.css';
 
 function App() {
+
+  const svg = d3.select("div")
+    .append("svg")
+    .attr("width", 50)
+    .attr("height", 100)
+
   return (
     <div className="App">
       
